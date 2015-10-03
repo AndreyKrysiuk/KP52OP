b7fc6bc27ae621cbcac1ba184742801e6383c1bd
AndreyKrysyuk <andrey130198@gmail.com>
2015-10-03 15:43:19 +0300

    math.c

--- courses/prog_base/labs/lab1/math.c
+++ courses/prog_base/labs/lab1/math.c
@@ -1,19 +1,18 @@
-#include<stdio.h>
-#include<stdlib.h>
 #include<math.h>
 
-double calc(double x, double y, double z)
+double calc(double x,double y, double z)
 {
-    double a,a0,a1,a2;
+      double a,a0,a1,a2;
+     if((x==y)||(x==0)||(z==0)||(sin(y)==0)||((x-y)<0)||(x<0)){
+        return NAN;
+    } else {
+
 
     a0=pow(x,y+1)/pow(x-y,1/z);
     a1=6*y +z/x;
-    a2=pow(x,1/abs(sin(y)));
+    a2=pow(x,1/fabs(sin(y)));
     a=a0+a1+a2;
-
-   if((x==y)||(z==0)||(x==0)||(sin(y)==0)){
-        return NAN;
-    } else {
     return a;
-    }
 }
+}
+
