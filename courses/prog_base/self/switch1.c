c83064b54983298ca235e1c384b0993154bbcadf
AndreyKrysyuk <andrey130198@gmail.com>
2015-10-03 14:00:49 +0300

    switch1

---
+++ courses/prog_base/self/switch1.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+int main()
+{
+int h = 2;
+int n = 15;
+int code=692;
+float price;
+if (code>=10&&code<=999){
+switch (code){
+case 44:
+price = (h*60 + n)*0.77;
+printf("%f\n",price);
+break;
+case 62:
+price = (h*60 + n)*0.8;
+printf("%f\n",price);
+break;
+case 32:
+price = (h*60 + n)*0.95;
+printf("%f\n",price);
+break;
+case 692:
+price = (h*60 + n)*1.5;
+printf("%f\n",price);
+break;
+case 697:
+price = (h*60 + n)*1.5;
+printf("%f\n",price);
+break;
+default:
+price = (h*60 + n)*1;
+printf("%f\n",price);
+break;
+}
+return 0;
+}
+}
