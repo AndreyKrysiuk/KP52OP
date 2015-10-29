0859de20544bb8d0df48d34ab0567764725a0e94
AndreyKrysyuk <andrey130198@gmail.com>
2015-10-29 22:38:16 +0200

    string_example

---
+++ courses/prog_base/tasks/string_example/proc.c
@@ -0,0 +1,36 @@
+#include <string.h>
+
+char * process(char * resultStr, const char * textLines[],
+               int linesNum, const char * extraStr) {
+    int i, j, k, check;
+    int extraLineNum;
+    const char * line;
+    char extraStrTrans[100];
+    puts("=== Text ===");
+    for(i = 0; i < linesNum; i++)
+        {
+        line = textLines[i];
+        puts(line);
+        }
+    puts("=== Extra string ===");
+    for(i = strlen(extraStr)-1, j=0; i >=0;j++, i--)
+    {
+        extraStrTrans[j]=extraStr[i];
+    }
+    extraStrTrans[j] = '\0';
+    puts(extraStrTrans);
+    for(i = 0; i < linesNum; i++){
+           check = strcmp(textLines[i], extraStrTrans);
+            if( check == 0){
+                extraLineNum  = i;
+                break;
+            }
+
+    }
+    if( check == 0){
+    sprintf(resultStr, "This is result: %i %s", extraLineNum, extraStrTrans);
+    } else {
+    sprintf(resultStr, "This is result: 0 NULL %s", extraStr);
+    }
+    return resultStr;
+}
