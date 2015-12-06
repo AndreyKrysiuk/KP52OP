ad4a59bca810a8acb1859dbe7d658684f2ec2e34
AndreyKrysyuk <andrey130198@gmail.com>
2015-12-06 19:15:02 +0200

    dyn_mem

--- courses/prog_base/tasks/dyn_mem/dyn_mem.c
+++ courses/prog_base/tasks/dyn_mem/dyn_mem.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 int main(){
     char *p = NULL;
     p = malloc(400 * sizeof(char));
     fgets(p, 400, stdin);
 
-    char *i;
-    int *count = 0;
-    char *lastxDec = malloc(sizeof(char));
+    int *i = malloc(sizeof(int));
+    int *count = malloc(sizeof(int));
+    *count=0;
+    int *lastxDec = malloc(sizeof(int));
 
-    for(i = p; *i != '\n'; i++){
-        if(isxdigit(*i) != 0){
-            lastxDec = i;
-            count++;
+    for(*(i) = 0; *(i) < strlen(p)-1; *(i)+=1){
+        if(isxdigit(p[*i]) != 0){
+            *lastxDec = *i;
+            *(count)+=1;
         }
     }
 
-    if(count != 0){
-        printf("Last symbol of hexadecimal number is %c", *lastxDec);
+    if(*count != 0){
+        printf("Last symbol of hexadecimal number is %c", p[*lastxDec]);
     } else {
     printf("0");
     }
@@ -28,5 +30,6 @@ int main(){
     free(p);
     return 0;
 }
+
 
 
