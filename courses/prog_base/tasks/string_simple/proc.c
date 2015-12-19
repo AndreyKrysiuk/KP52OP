8d8fc47a865c0a40f90dc31f7319b7d068e659b7
AndreyKrysyuk <andrey130198@gmail.com>
2015-12-19 20:43:14 +0200

    string_simple

--- courses/prog_base/tasks/string_simple/proc.c
+++ courses/prog_base/tasks/string_simple/proc.c
@@ -1,34 +1,26 @@
 #include <string.h>
 #include <stdio.h>
 
-char * process(char * resultStr, const char * textLines[],
-               int linesNum, const char * extraStr) {
-    int i, j, k, check;
+char * process(char * resultStr, const char * textLines[], int linesNum, const char * extraStr){
+    int check;
     int extraLineNum;
-    const char * line;
-    char extraStrTrans[100];
-    for(i = 0; i < linesNum; i++)
-        {
-        line = textLines[i];
+    int k = 0;
+    int i;
+    char extraStrInverse[100] = "\0";
 
-        }
-    for(i = strlen(extraStr)-1, j=0; i >=0;j++, i--)
-    {
-        extraStrTrans[j]=extraStr[i];
+    for (i = strlen(extraStr)-1; i >= 0; i--){
+        extraStrInverse[k++] = extraStr[i];
     }
-    extraStrTrans[j] = '\0';
+
     for(i = 0; i < linesNum; i++){
-           check = strcmp(textLines[i], extraStrTrans);
+           check = strcmp(textLines[i], extraStrInverse);
             if( check == 0){
                 extraLineNum  = i;
+                sprintf(resultStr, "This is result: %i %s", extraLineNum, extraStrInverse);
                 break;
+            } else {
+                sprintf(resultStr, "This is result: 0 NULL %s", extraStrInverse);
             }
-
-    }
-    if( check == 0){
-    sprintf(resultStr, "This is result: %i %s", extraLineNum, extraStrTrans);
-    } else {
-    sprintf(resultStr, "This is result: 0 NULL %s", extraStr);
     }
     return resultStr;
 }
