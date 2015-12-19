334ef08ed282c6ba5d2144f90c6d9a602b0153b2
AndreyKrysyuk <andrey130198@gmail.com>
2015-12-19 21:06:55 +0200

    long2double

--- courses/prog_base/tasks/long2double/repr.c
+++ courses/prog_base/tasks/long2double/repr.c
@@ -1,75 +1,40 @@
 #include<math.h>
 
-double long2double(long long n){
-	int i;
-	int charn;
-	int exp, E = 0, sign;
-	double F = 0;
-	double result = 0;
-	char binary[64] = "";
-	long long n1;
-	for (charn = 63; charn >= 0; charn--)
-    {
-		n1 = n;
-		n1 = n1 >> 1;
-		n1 = n1 << 1;
-		if (n==n1)
-            {
-            binary[charn]='0';
-            } else {
-                binary[charn]='1';
-            }
-		n = n >> 1;
-	}
-	sign = binary[0];
-	for (charn = 12, exp = -1; charn <= 63; charn++, exp--)
+double long2double(long long num){
+    unsigned long long mask = 1;
+	mask <<= 63;
+	int count = 0;
+	int sign = 0;
+	short int e = 0;
+	double m = 0;
+	if (mask == (num&mask))
+		sign = 1;
+	mask >>= 1;
+	count++;
+	int powN = 10;
+	for (count; count < 12; count++)
 	{
-		if (binary[charn] == '1')
-            {
-			F= F + pow(2, exp);
-            }
+		if (mask == (num&mask))
+			e += pow(2, powN);
+		powN--;
+		mask >>= 1;
 	}
-	for (charn = 11, exp = 0; charn >= 1; charn--, exp++)
-        {
-		if (binary[charn] == '1')
-		{
-			E = E + pow(2, exp);
-		}
+	for (count; count < 64; count++)
+	{
+		if (mask == (num&mask))
+			m += pow(2, powN);
+		powN--;
+		mask >>= 1;
 	}
-	if ((E == 0) && (F != 0))
-        {
-		result = (1+F)*pow(2, -1022);
-            if(sign == 1)
-            {
-                result = -result;
-            }
-		}
-
-	else {
-		result = (1+F)*pow(2, E - 1023);
-            if(sign == 1)
-                {
-                result = -result;
-                }
-        }
-    if ((E == 0)&& (F == 0))
-        { if(sign == 0){
-        result = +0;
-        } else {
-        result = -0;}
-
-    }
-
-	if ((E == 2047) && (F!= 0) && (binary[0] == '0'))
-        {
-        return NAN;
-        }
-    if ((E == 2047)&&(F==0)){
-        if(sign == 0){
-            result = +INFINITY;
-        } else {
-        result = -INFINITY;}
-    }
-
-	return result;
+	if (e == 2047 && m > 0)
+		return pow(-1, sign)*NAN;
+	else if (e == 2047)
+		return pow(-1, sign)*INFINITY;
+	else if (e == 0 && m == 0)
+		return pow(-1, sign)*0.0;
+	else if (e == 0 && m > 0)
+		return pow(-1, sign)*pow(2,-1022) * (m);
+	else if(e > 0 && e < 2047)
+		return pow(-1, sign)*pow(2, e - 1023) * (1 + m);
 }
+
