fc9e6bec25eeeefb313d7109f809f5a6de4ed475
AndreyKrysyuk <andrey130198@gmail.com>
2016-04-08 18:11:53 +0300

    Update car.c

--- courses/prog_base_2/labs/lab1/car.c
+++ courses/prog_base_2/labs/lab1/car.c
@@ -6,25 +6,27 @@ struct car_s{
     char carNumber[CAR_NUMBER];
 };
 
-car_t car_new(){
-    car_t car = (car_t)malloc(sizeof(struct car_s));
-    char carNumber[CAR_NUMBER];
-    for(int i = 0; i < CAR_NUMBER - 1; i++){
-        switch(i){
-            case 0:
-            case 1:
-            case 6:
-            case 7:
-                carNumber[i] = (char)(65+ rand()%26);
-                break;
-            default:
-                carNumber[i] = (char)(48 + rand()%10);
-                break;
-            }
-    }
-    strcpy(car->carNumber, carNumber);
-    car->speed = 1 + rand()%200;
-    return car;
+car_t car_new(int speed){
+    if(speed > 0 && speed < 600){
+        car_t car = (car_t)malloc(sizeof(struct car_s));
+        char carNumber[CAR_NUMBER];
+        for(int i = 0; i < CAR_NUMBER - 1; i++){
+            switch(i){
+                case 0:
+                case 1:
+                case 6:
+                case 7:
+                    carNumber[i] = (char)(65+ rand()%26);
+                    break;
+                default:
+                    carNumber[i] = (char)(48 + rand()%10);
+                    break;
+                }
+        }
+        strcpy(car->carNumber, carNumber);
+        car->speed = speed;
+        return car;
+    } else return NULL;
 }
 
 int car_getSpeed(car_t car){
