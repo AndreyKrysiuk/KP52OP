3bcd7c215158b6612b1e77901692b9b6bbf312a6
AndreyKrysyuk <andrey130198@gmail.com>
2016-04-08 18:14:36 +0300

    Update main.c

--- courses/prog_base_2/labs/lab1/main.c
+++ courses/prog_base_2/labs/lab1/main.c
@@ -11,39 +11,13 @@
 #include "car.h"
 
 
-/*int main()
-{
-    srand(time(NULL));
-    car_t car1 = car_new();
-    car_t car2 = car_new();
-    printf("%s  speed: %d\n", car_getNumber(car1), car_getSpeed(car1));
-    printf("%s  speed: %d\n", car_getNumber(car2), car_getSpeed(car2));
-
-    posts_t system_posts = posts_new();
-    posts_addNewPost(system_posts, 120);
-    posts_addNewPost(system_posts, 60);
-    posts_printInfoAboutPosts(system_posts);
-    core_t system_core = core_new();
-
-    if((posts_checkViolator(system_posts, 0, car1, system_core) == 0 || posts_checkViolator(system_posts, 1, car2, system_core) == 0)){
-        core_printViolatorsList(system_core);
-    } else {
-        printf("No violators");
-    }
-    car_remove(car1);
-    car_remove(car2);
-    posts_remove(system_posts);
-    core_remove(system_core);
-}*/
-
-static void posts_new_void_ZeroCountPosts(void ** state){
+static void posts_new__void__ZeroCountPosts(void ** state){
     posts_t posts = posts_new();
     assert_int_equal(posts_getAmountPosts(posts), 0);
     assert_int_equal(posts_getErrorCode(posts), 0);
     posts_remove(posts);
 }
-
-static void posts_addNewPost_tenNewPosts_CountPostsTen(void ** state){
+static void posts_addNewPost__tenNewPosts__CountPostsTen(void ** state){
     posts_t posts = posts_new();
     for(int i = 0; i < 10; i++){
         posts_addNewPost(posts, (i+1)*10);
@@ -56,16 +30,23 @@ static void posts_addNewPost_tenNewPosts_CountPostsTen(void ** state){
     posts_remove(posts);
 }
 
+static void core_new__void__ZeroCountViolators(void ** state){
+    core_t core = core_new();
+    assert_int_equal(core_getAmountViolators(core), 0);
+    assert_int_equal(core_getErrorStatus(core), 0);
+    core_remove(core);
+}
+
 
 
 
 int main(){
     {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(posts_new_void_ZeroCountPosts),
-        cmocka_unit_test(posts_addNewPost_tenNewPosts_CountPostsTen),
-/*        cmocka_unit_test(),
-        cmocka_unit_test(),
+        cmocka_unit_test(posts_new__void__ZeroCountPosts),
+        cmocka_unit_test(posts_addNewPost__tenNewPosts__CountPostsTen),
+        cmocka_unit_test(core_new__void__ZeroCountViolators),
+        /*cmocka_unit_test(),
         cmocka_unit_test(),
         cmocka_unit_test(),
         cmocka_unit_test(),
@@ -76,7 +57,7 @@ int main(){
         cmocka_unit_test(),
         cmocka_unit_test(),
         cmocka_unit_test()*/
-    };
+   };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
 }
